c727cae297035cd7c5d82c3e72f317983a90626a
Liam Deacon <liam.deacon@lightbytestechnology.co.uk>
2020-10-10 10:07:50 +0100

    Add some vues and generically configure Router

--- online_store/frontend/src/router/index.js
+++ online_store/frontend/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+const routerOptions = [
+  { path: '/', component: 'Home' },
+  { path: '/about', component: 'About' }
+]
+
+const routes = routerOptions.map(route => {
+  return {
+    ...route,
+    name: `${route.component}`,
+    component: () => import(`@/components/${route.component}.vue`)
+  }
+})
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes: routes,
+  mode: 'history'
 })
